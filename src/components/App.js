97010c23cb18cc747d55ce79b31f523066eea8c2
Rajitha9573 <rajithabhukya967@gmail.com>
2024-01-26 11:55:40 +0530

    new changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Fetch from "./fetch";
 
 const App = () => {
   return (
     <div>
+      <Fetch />
         {/* Do not remove the main div */}
+        
     </div>
   )
 }
---
+++ src/components/Fetch.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const Fetch = () => {
+  const [user, setUser] = useState();
+
+  
+
+  function fetchData() {
+    // API endpoint URL
+    const apiUrl = "https://reqres.in/api/users";
+
+    fetch(apiUrl)
+      .then((response) => response.json()) // Add return statement here
+      .then((data) =>
+      // console.log(data.data),
+      setUser(data.data)
+      
+      
+      )
+      .catch(
+        (error) => 
+        console.error("Error fetching data:", error)
+      ); // Add error handling
+      console.log(user)
+  }
+
+  return (
+    <div>
+      <button onClick={fetchData} className="btn">
+        Get User List
+      </button>
+      <table >
+        
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        
+          <tbody>
+           
+            {
+
+              user && user.map(value => 
+            
+
+
+            <tr>
+              <td>{value.first_name}</td>
+              <td>{value.last_name}</td>
+              <td>{value.email}</td>
+              <td>
+              <img src={value.avatar} alt={value.last_name} />
+              </td>
+            
+            </tr>
+              )
+              
+}
+
+          </tbody>
+        
+      </table> 
+    </div>
+  );
+};
+
+export default Fetch;
